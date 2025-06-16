b59443a764db4dabd954e6a8c91dad69cd28d989
CreativeCrustacean <pokemongirlva200@gmail.com>
2025-06-16 14:54:07 -0400

    Add project files.

---
+++ IDE and VCS Workflow Project P1/IDE and VCS Workflow Project P1.cpp
@@ -0,0 +1,11 @@
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+#include <iostream>
+using namespace std;
+int main() {
+    char first, middle, last;
+    first = 'L';
+    middle = 'C';
+    last = 'J';
+    cout << first << middle << last << endl;
+    return 0;
+}
